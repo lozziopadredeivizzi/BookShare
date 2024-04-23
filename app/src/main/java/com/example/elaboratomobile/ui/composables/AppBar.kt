fef7174458e5e9c2a10365e2bbc5d96f38d23e42
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-04-23 15:43:30 +0200

    Create App Bar

---
+++ app/src/main/java/com/example/elaboratomobile/ui/composables/AppBar.kt
@@ -0,0 +1,49 @@
+package com.example.elaboratomobile.ui.composables
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.example.elaboratomobile.ui.BookShareRoute
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    navController: NavHostController,
+    currentRoute: BookShareRoute
+) {
+    if(!BookShareRoute.noAppBar.contains(currentRoute)) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    currentRoute.title,
+                    fontWeight = FontWeight.Medium,
+                )
+            },
+            navigationIcon = {
+                if (currentRoute.route != BookShareRoute.Login.route) {
+                    Icon(
+                        imageVector = Icons.Outlined.ArrowBack,
+                        contentDescription = "Back button"
+                    )
+                }
+            },
+            actions = {
+
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer
+            )
+        )
+    }
+}
