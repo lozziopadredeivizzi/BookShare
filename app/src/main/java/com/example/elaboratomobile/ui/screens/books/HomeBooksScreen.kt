e918a6fb934a7bb6c1194954188911ae5cb34fae
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-04-25 18:47:35 +0200

    Fixed home card layout

--- app/src/main/java/com/example/elaboratomobile/ui/screens/books/HomeBooksScreen.kt
+++ app/src/main/java/com/example/elaboratomobile/ui/screens/books/HomeBooksScreen.kt
@@ -3,9 +3,11 @@ package com.example.elaboratomobile.ui.screens.books
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,10 +19,13 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.Image
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,6 +38,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.elaboratomobile.R
+import com.example.elaboratomobile.ui.composables.RatingBarNoClick
 
 @Composable
 fun HomeBooksScreen(navController: NavHostController) {
@@ -56,39 +62,65 @@ fun BookItem(item: Int, onClick: () -> Unit) {
         onClick = onClick,
         modifier = Modifier
             .padding(
-                horizontal = 30.dp,
+                horizontal = 16.dp,
                 vertical = 8.dp
             )
             .width(70.dp)
-            .height(150.dp),
+            .height(200.dp),
         colors = CardDefaults.cardColors(
             containerColor = Color.White
         ),
         border = BorderStroke(1.dp, Color.Blue)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(horizontal = 30.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.copertina),
-                "Cover",
-                contentScale = ContentScale.Fit,
-                modifier = Modifier.height(100.dp)
-                    .padding(horizontal = 30.dp)
-            )
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(30.dp)
+        ) { // Box che riempie l'intera area disponibile
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(text = "Titolo")
-                Text(text = "Autore")
-                Text(text = "Genere")
+                Image(
+                    painter = painterResource(id = R.drawable.copertina),
+                    contentDescription = "Cover",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .height(130.dp)
+                        .padding(end = 30.dp)
+                )
+                Column(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .weight(1f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Titolo")
+                    Text(text = "Autore")
+                    Text(text = "Genere")
+                    RatingBarNoClick(rating = 3.5)
+                }
+            }
+            Spacer(modifier = Modifier.size(8.dp))
+            // Posizionamento dell'IconButton
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .size(25.dp)
+
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Favorite,
+                    contentDescription = "Preferito",
+                    tint = Color.Red
+                )
             }
         }
+
     }
+
 }
