7f93db3c1ac63cbb43cff4ffd1fa0426064ffb48
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-05-01 19:49:42 +0200

    Unificate AppModule

--- app/src/main/java/com/example/elaboratomobile/AppModule.kt
+++ app/src/main/java/com/example/elaboratomobile/AppModule.kt
@@ -4,18 +4,24 @@ import android.content.Context
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.room.Room
 import com.example.elaboratomobile.data.database.ElaboratoMobileDatabase
+import com.example.elaboratomobile.data.repositories.AspettoRepository
 import com.example.elaboratomobile.data.repositories.BooksRepository
 import com.example.elaboratomobile.data.repositories.UsernameRepository
 import com.example.elaboratomobile.data.repositories.UtenteRepository
+import com.example.elaboratomobile.ui.screens.aspetto.AspettoViewModel
 import com.example.elaboratomobile.ui.screens.login.LoginViewModel
 import com.example.elaboratomobile.ui.screens.share.BooksViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val Context.dataStore by preferencesDataStore("currentUsername")
+val Context.usernameDataStore by preferencesDataStore("currentUsername")
+val Context.themeDataStore by preferencesDataStore("theme")
+
 
 val appModule = module {
-    single { get<Context>().dataStore }
+    single { get<Context>().usernameDataStore }
+
+    single { get<Context>().themeDataStore }
 
     single {
         Room.databaseBuilder(
@@ -30,6 +36,10 @@ val appModule = module {
     single { UtenteRepository(get<ElaboratoMobileDatabase>().utenteDAO()) }
 
     single { BooksRepository(get<ElaboratoMobileDatabase>().libroDAO()) }
+
+    single { AspettoRepository(get()) }
+
+    viewModel { AspettoViewModel(get()) }
 
     viewModel { LoginViewModel(get(), get()) }
 
