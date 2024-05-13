a35bc79d04127b6e37f709496acb30b3e6cabcab
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-05-13 15:04:35 +0200

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,9 +52,9 @@ android {
 
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -64,7 +64,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation("io.insert-koin:koin-androidx-compose:3.5.3")
     val roomVersion = "2.6.1"
