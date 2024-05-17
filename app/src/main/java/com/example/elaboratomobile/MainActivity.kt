4fe1da8fef77e9acbe74086b4a9018e5119b257c
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-05-17 11:27:27 +0200

    Implemented biometric prompt

--- app/src/main/java/com/example/elaboratomobile/MainActivity.kt
+++ app/src/main/java/com/example/elaboratomobile/MainActivity.kt
@@ -1,23 +1,18 @@
 package com.example.elaboratomobile
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -29,12 +24,15 @@ import com.example.elaboratomobile.ui.composables.BottomBar
 import com.example.elaboratomobile.ui.screens.aspetto.AspettoViewModel
 import com.example.elaboratomobile.ui.screens.notification.NotificationViewModel
 import com.example.elaboratomobile.ui.theme.ElaboratoMobileTheme
+import com.example.elaboratomobile.utils.BiometricPromptManager
 import com.example.elaboratomobile.utils.LocationService
 import org.koin.android.ext.android.get
 import org.koin.androidx.compose.koinViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private val promptManager by lazy {
+        BiometricPromptManager(this)
+    }
     private lateinit var locationService: LocationService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -88,7 +86,8 @@ class MainActivity : ComponentActivity() {
                             navController = navController,
                             modifier = Modifier.padding(contentPadding),
                             themeState,
-                            themeViewModel::changeTheme
+                            themeViewModel::changeTheme,
+                            promptManager
                         )
                     }
 
